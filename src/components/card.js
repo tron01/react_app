75413ac139aeee2ab47ff460cc1eb303dea4e74e
tron01 <jangoovasuu0789@gmail.com>
2022-10-26 21:05:46 +0530

    js passing to jsx by {}

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,11 @@
 function Card() {
+    let x = 1;
     return (
         <div>
             <div className="card xl-2 " >
                 <div className="card-body">
                     This is some text within a card body.
+                    {x}
                 </div>
             </div>
         </div>
