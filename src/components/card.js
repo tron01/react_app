5cd713b00929eac3c62732f9134b6ab5c69a8041
tron01 <jangoovasuu0789@gmail.com>
2022-10-26 21:19:48 +0530

    state added using hooks

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,16 @@
+import React, { useState } from 'react';
 function Card() {
-    let x = 1;
+    const [x, setX] = useState(0);
+
+    const btnFn = () => {
+        setX(x + 1);
+    }
     return (
         <div>
             <div className="card xl-2 " >
                 <div className="card-body">
-                    This is some text within a card body.
-                    {x}
+                    <button onClick={btnFn} className="btn btn-danger"> press</button><br></br>
+                    The number =  {x}
                 </div>
             </div>
         </div>
