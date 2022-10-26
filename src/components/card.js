7f34bc344a4bc48c60b8163694fd2bc12487ff0e
tron01 <jangoovasuu0789@gmail.com>
2022-10-26 21:04:33 +0530

    card added

---
+++ src/components/card.js
@@ -0,0 +1,12 @@
+function Card() {
+    return (
+        <div>
+            <div className="card xl-2 " >
+                <div className="card-body">
+                    This is some text within a card body.
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Card;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Navbar from './components/navbar';
 import Foot from './components/footer';
+import Card from './components/card';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Card />
     <Foot />
   </React.StrictMode>
 );
