8a85bd505b5e2cb7b97ae762b42a1ceb446458a9
tron01 <jangoovasuu0789@gmail.com>
2022-10-28 22:14:30 +0530

    form submit input

--- src/components/form.js
+++ src/components/form.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 function Myform() {
     const [age, setage] = useState(" ");
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -11,10 +14,15 @@ function Myform() {
                         <input type="text" value={age} onChange={(e) => {
                             setage(e.target.value);
                         }}></input>
+                        <button type="submit" className="btn btn-info">submit</button>
+
                     </div>
+                    <br />
+                    <span>Age: { }</span>
                 </div>
             </form>
 
         </div>
     );
 }
+export default Myform;
