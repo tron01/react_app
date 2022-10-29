a28ea17160b26fd85386bcc3ab10aabd786f1e92
tron01 <jangoovasuu0789@gmail.com>
2022-10-29 15:37:40 +0530

    classfix

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,7 +20,7 @@ function Navbar() {
                                 <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
                                 </a>
-                                <ul class="dropdown-menu">
+                                <ul className="dropdown-menu">
                                     <li><a className="dropdown-item" href="#">Action</a></li>
                                     <li><a className="dropdown-item" href="#">Another action</a></li>
                                     <li><hr className="dropdown-divider" /></li>
@@ -31,7 +31,7 @@ function Navbar() {
                                 <a className="nav-link disabled">Disabled</a>
                             </li>
                         </ul>
-                        <form class="d-flex" role="search">
+                        <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
