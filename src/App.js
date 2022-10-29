146f5554276c85b58d258dede33dc311dffb7d34
tron01 <jangoovasuu0789@gmail.com>
2022-10-29 15:37:03 +0530

    form prop added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Card from './components/card';
+import Form from './components/form';
+
 
 function App() {
+  const onFormSubmit = (a) => {
+    console.log(a);
+  }
   return (
     <div>
       <Card />
+      <Form onFormSubmit={onFormSubmit} />
     </div>
   );
 }
