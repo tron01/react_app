11396c837b38e8f30ba01188bc9728be6919050e
tron01 <jangoovasuu0789@gmail.com>
2022-10-30 19:20:34 +0530

    Class component

---
+++ src/components/list.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+let a = "sccac";
+class List extends Component {
+
+
+    render() {
+        return <div>
+            <ul>
+                <li>{a}</li>
+            </ul>
+        </div>
+    }
+}
+export default List;
