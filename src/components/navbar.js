908c4b5ea498ed58e0fbc9a1c239fec72fa516f9
Abhi3070 <69385258+tron01@users.noreply.github.com>
2022-11-26 15:30:06 +0530

    nav fix href

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,14 +4,14 @@ function Navbar() {
         <div>
             <nav className="navbar navbar-expand-lg bg-danger shadow-lg p-3 mb-5 bg-body rounded">
                 <div className="container-fluid">
-                    <a className="navbar-brand" >Navbar</a>
+                    <a className="navbar-brand" href="" >Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="">Home</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="">Link</a>
